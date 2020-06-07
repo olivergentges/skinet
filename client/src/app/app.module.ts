9b34c90e4ed9ec8b0c9d7f9a1d58c83650bba776
Oliver Gentges <oliver.gentges@t-online.de>
2020-06-07 14:18:49 +0200

    mit font

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
